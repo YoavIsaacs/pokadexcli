5aee320bf2b07c95ad5d6d3020df4a7fae68947c
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-02-18 22:50:09 +0200

    created test for 1st function

---
+++ repl_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "    hello world   ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "poopoo poop                 poo",
+			expected: []string{"poopoo", "poop", "poo"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Error: expected %d words, got %d", len(c.expected), len(actual))
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Error: expected %d words, got %d", len(c.expected), len(actual))
+			}
+		}
+	}
+}
