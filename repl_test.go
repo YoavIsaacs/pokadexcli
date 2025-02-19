7f88088b9081e6edfa4727a3680ffb8e8cd05125
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-02-19 15:30:56 +0200

    added test for cache

--- repl_test.go
+++ repl_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"testing"
+	"time"
+
+	"github.com/YoavIsaacs/pokadexcli/internal/pokecache"
 )
 
 func TestCleanInput(t *testing.T) {
@@ -31,5 +34,27 @@ func TestCleanInput(t *testing.T) {
 				t.Errorf("Error: expected %d words, got %d", len(c.expected), len(actual))
 			}
 		}
+	}
+}
+
+func TestCacheAddAndGet(t *testing.T) {
+	cache := pokecache.NewCache(5 * time.Second)
+	testKey := "hello"
+	testData := []byte("test-data")
+
+	cache.Add(testKey, testData)
+
+	data, err := cache.Get(testKey)
+	if err != nil {
+		t.Errorf("Failed to get item from cache: %v", err)
+	}
+
+	if string(data) != string(testData) {
+		t.Errorf("Cache returned wrong data. Expected %s, got %s", string(testData), string(data))
+	}
+
+	_, err = cache.Get("Non-existent")
+	if err == nil {
+		t.Error("Expected error when getting non-existent key, got nil")
 	}
 }
